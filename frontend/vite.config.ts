82b02c60b19a63f579716555beb6327e5cd63351
sojipi <zojswb@163.com>
2026-02-09 23:04:28 +0800

    vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,11 @@ export default defineConfig({
     server: {
         port: 3000,
         host: true,
+        allowedHosts: [
+            'match.zenmb.com',
+            'localhost',
+            '127.0.0.1',
+        ],
         proxy: {
             '/api': {
                 target: 'http://localhost:8000',
